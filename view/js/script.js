a1e7e5b6531ec5cdff5ca5c824a6dfdcd5c018f1
zhpadvoiski <zh.padvoiski@gmail.com>
2021-10-20 22:50:43 -0500

    data fetch

--- view/js/script.js
+++ view/js/script.js
@@ -1,4 +1,35 @@
 import loader from "./loader.js";
 
-//API `https://jsonmock.hackerrank.com/api/transactions?userId=${id}&page=${page}`
 loader.hideLoader();
+
+//API `https://jsonmock.hackerrank.com/api/transactions?userId=${id}&page=${page}`
+const showBtn = document.getElementById("submit-btn");
+const userSelect = document.getElementById("user-select");
+
+showBtn.addEventListener("click", getTransactions);
+
+async function getTransactions(id) {
+	const userValue = userSelect.value;
+	if (!userSelect) {
+		alert("You need to select user to fetch data!");
+		return;
+	}
+
+	const URL = getURL(userValue);
+	const data = await fetchData(URL);
+	debugger;
+}
+
+function getURL(id, page = 1) {
+	return `https://jsonmock.hackerrank.com/api/transactions?userId=${id}&page=${page}`;
+}
+
+async function fetchData(url) {
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		return data;
+	} catch (e) {
+		console.log(e);
+	}
+}
